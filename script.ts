39e15347fe6ad4ebe3b83fed843a368075be9cc8
Vera Kippel <vera.kippel@outlook.com>
2025-03-31 22:06:49 +0700

    Lägegr in variabler

--- script.ts
+++ script.ts
@@ -4,3 +4,11 @@ interface CourseInfo{
     progression: "A" | "B" | "C",
     syllabus: string
 }
+
+//Variabler
+let courseList = document.getElementById("course-list") as HTMLLIElement;
+let form = document.getElementById("form-courses") as HTMLFormElement;
+let codeValue = document.getElementById("code") as HTMLInputElement;
+let nameValue = document.getElementById("name") as HTMLInputElement;
+let progressionValue = document.getElementById("progression") as HTMLSelectElement;
+let syllasbusValue = document.getElementById("syllabus") as HTMLInputElement;
